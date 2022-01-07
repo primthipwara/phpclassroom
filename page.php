122b11ee52fadf3dbacb5aec542977bda26f62b4
lsw_56418 <89379617+primthipwara@users.noreply.github.com>
2022-01-07 14:02:34 +0700

    Update page.php

--- page.php
+++ page.php
@@ -2,11 +2,24 @@
   <head>
     <style>
       body {
-        background-color: yellow;
+        background-color: #D3D3D3;
+      }
+      #box1{
+        width: 500px;
+        height: 500px;
+        background-color: #FFDAB9;
+      }
+      .box2{
+        width: 500px;
+        height: 500px;
+        background-color: #ADD8E6;
       }
     </style>
   </head>
   <body>
-  
+    <div id="box1">
+    </div>
+    <div class="box2">
+    </div>
   </body>
 </html>
