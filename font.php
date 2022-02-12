45598460d45c934594f1c2c012333f1ae1ae6721
lsw_56418 <89379617+primthipwara@users.noreply.github.com>
2022-02-12 21:58:26 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,1 +1,29 @@
-
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(SOV_poster.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(lookpeach.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นางสาว ทิพย์วรา เกษามูล เลขที่ 20 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นางสาว ทิพย์วรา เกษามูล เลขที่ 20 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
